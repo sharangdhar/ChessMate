8fea8ca8a7591a2148d3432fb80725353f5bbd8d
Sharangdhar Chakraborty <sharangdharc@yahoo.com>
2015-03-26 23:04:07 -0400

    init structure

---
+++ back/server.py
@@ -0,0 +1,63 @@
+#! /usr/bin/env python
+
+import BaseHTTPServer
+from BaseHTTPServer import BaseHTTPRequestHandler
+
+from os import curdir, sep
+import cgi, sys, re, os, json, zipfile, subprocess, random
+
+
+PORT = '9001'
+tid = 0
+
+class handler (BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        print self.path
+        if self.path=="/":
+            self.path="/front/index.html"
+        else:
+            self.path="/front" + self.path
+
+        print self.path
+
+        try:
+            mime = None
+            if self.path.endswith(".html"):
+                mime='text/html'
+            if self.path.endswith(".js"):
+                mime='application/javascript'
+            if self.path.endswith(".jpg"):
+                mime='image/jpg'
+            if self.path.endswith(".gif"):
+                mime='image/gif'
+            if self.path.endswith(".png"):
+                mime='image/png'
+            if self.path.endswith(".css"):
+                mime='text/css'
+
+            if mime:
+                file = open(curdir + sep + self.path) 
+                self.send_response(200)
+                self.send_header('Content-type',mime)
+                self.end_headers()
+                self.wfile.write(file.read())
+                file.close()
+                return
+
+        except IOError:
+            self.send_error(404,'File Not Found: %s' % self.path)
+
+    #make doPOST method
+
+
+try:
+    server = BaseHTTPServer.HTTPServer(('', 9001), handler)
+    print 'Starting ChessMate server on port ' + PORT + '!\n'
+    server.serve_forever()
+
+except KeyboardInterrupt:
+    print 'Exiting...\nThank you for using the server! Have a pleasant day :)!\n'
+    os.system("rm -rf data/env/* data/checks/* data/results/*")
+    server.socket.close()
+    exit(0)
