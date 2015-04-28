83cdde859e6ae91ccdb1b1fc91604b2f81085ae4
Ryan Archer <ryanarcher@ryans-air-3.wv.cc.cmu.edu>
2015-04-28 15:46:20 -0400

    Added a puzzle and a button to play this puzzle

---
+++ front/puzzles/puzzle1.js
@@ -0,0 +1,14 @@
+function nextPuzzle() {
+    board2.position({
+      a2: 'bR',
+      f2: 'wP',
+      g2: 'wK',
+      h2: 'wP',
+      e3: 'wR',
+      g3: 'wP',
+      f7: 'bP',
+      g7: 'bP',
+      h7: 'bP',
+      g8: 'bK'
+    });
+}
