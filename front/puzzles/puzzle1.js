28b6b366b59f733503a368cc2309df197eb8daaa
Ryan Archer <ryanarcher@ryans-air-3.wv.cc.cmu.edu>
2015-04-29 01:54:19 -0400

    Added the three puzzles described in how to play

--- front/puzzles/puzzle1.js
+++ front/puzzles/puzzle1.js
@@ -1,14 +1,12 @@
+var puzzle_array = [{a4: 'bK', c4: 'wK', a6: 'wR'}, 
+                    {a2: 'wR', c2: 'wR', e1: 'wK', g3: 'bP',
+                     h3: 'bQ', h4: 'bK'},
+                    {a8: 'wK', d8: 'wB', f8: 'bK', e6: 'wB'} ];
+
 function nextPuzzle() {
-    board2.position({
-      a2: 'bR',
-      f2: 'wP',
-      g2: 'wK',
-      h2: 'wP',
-      e3: 'wR',
-      g3: 'wP',
-      f7: 'bP',
-      g7: 'bP',
-      h7: 'bP',
-      g8: 'bK'
-    });
+  counter += 1;
+  if(counter >= puzzle_array.length) {
+    counter = 0;
+  }
+  board2.position(puzzle_array[counter]);
 }
